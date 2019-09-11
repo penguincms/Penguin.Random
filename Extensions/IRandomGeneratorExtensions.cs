57012b3282835ef219fd67e88b66b057e03ba122
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-09-11 12:06:23 -0700

    Add files via upload

---
+++ Extensions/IRandomGeneratorExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Penguin.Math.Extensions;
+using Penguin.Random.Interfaces;
+
+namespace Penguin.Random.Extensions
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public static class IRandomGeneratorExtensions
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+    {
+        /// <summary>Selects an ulong beneath the set value</summary>
+        /// <param name="rng">The source RNG</param>
+        /// <param name="MaxValue">The max value to return (Exclusive)</param>
+        /// <returns>The next value in the sequence below the max value</returns>
+        public static ulong Next(this IRandomGenerator rng, ulong MaxValue)
+        {
+            return rng.Next(0UL, MaxValue);
+        }
+
+        /// <summary>Selects an ulong between the two values</summary>
+        /// <param name="rng">The source RNG</param>
+        /// <param name="MinValue">The Minimum value possible (Inclusive)</param>
+        /// <param name="MaxValue">The Maximum value possible (Exclusive)</param>
+        /// <returns>The next value in the sequence between the given values</returns>
+        public static ulong Next(this IRandomGenerator rng, ulong MinValue, ulong MaxValue)
+        {
+            ulong x = rng.Next();
+
+            while (!x.Between(MinValue, MaxValue)) { 
+                x = rng.Next();
+            }
+
+            return x;
+        }
+    }
+}
---
+++ Interfaces/IRandomGenerator.cs
@@ -0,0 +1,20 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Penguin.Random.Interfaces.IRandomGenerator
+// Assembly: Penguin.Random, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 4A5518E0-DEF6-4913-9205-059E59B1CA91
+// Assembly location: C:\Git\MassageEnvy.com Umbraco 8\MassageEnvy\bin\Penguin.Random.dll
+
+namespace Penguin.Random.Interfaces
+{
+  /// <summary>An interface used to define RNG extension methods</summary>
+  public interface IRandomGenerator
+  {
+    /// <summary>Gets the next ulong from the RNG</summary>
+    /// <returns></returns>
+    ulong Next();
+
+    /// <summary>Gets the next double from the RNG</summary>
+    /// <returns></returns>
+    double NextDouble();
+  }
+}
